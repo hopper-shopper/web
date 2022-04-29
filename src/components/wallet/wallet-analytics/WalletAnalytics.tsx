6831529fd4b3282e8813c45e744371289590558f
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2022-04-29 23:12:52 +0200

    HOPS-26: Feature idea text

--- src/components/wallet/wallet-analytics/WalletAnalytics.tsx
+++ src/components/wallet/wallet-analytics/WalletAnalytics.tsx
@@ -1,3 +1,4 @@
+import EmptyText from "components/typography/empty-text/EmptyText"
 import { WalletAddress } from "models/User"
 import { Screens, styled } from "theme"
 import ClaimedChartCard from "../claimed-chart-card/ClaimedChartCard"
@@ -12,6 +13,18 @@ export default function WalletAnalytics(props: WalletAnalyticsProps) {
     return (
         <Container>
             <ClaimedChartCard wallet={wallet} />
+
+            <FutureText>
+                More to come
+                <br />
+                <br />
+                You have an chart idea or a feature request in general?
+                <br />
+                Write me on Discord @ steschwa#3314 or{" "}
+                <StyledLink href="https://twitter.com/0x_steschwa" target="_blank">
+                    Twitter
+                </StyledLink>
+            </FutureText>
         </Container>
     )
 }
@@ -22,4 +35,14 @@ const Container = styled("div", {
     gap: "2rem",
     maxWidth: Screens.xl,
     margin: "3rem auto",
+})
+const FutureText = styled("p", {
+    color: "$gray11",
+    textAlign: "center",
+    lineHeight: 1.5,
+})
+const StyledLink = styled("a", {
+    textDecoration: "none",
+    color: "$blue11",
+    fontSize: "1rem",
 })
